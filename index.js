1bb949346a8f84d241d60fef6baa548a01984ebb
Arifjawad18 <arif.jawad18@gmail.com>
2022-11-20 02:57:10 +0600

    orders addall and finish

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ async function run(){
         app.get('/services/:id', async(req, res) =>{
             const id = req.params.id;
             const query = {_id: ObjectId(id) };
-            const service = await serviceCollection.findOne(query);
-            res.send(service)
+            const services = await serviceCollection.findOne(query);
+            res.send(services)
 
         });
         // orders api //
