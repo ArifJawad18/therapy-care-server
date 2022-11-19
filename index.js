c721366f396aab4eeb7a0371f0cbfc0093a64b40
Arifjawad18 <arif.jawad18@gmail.com>
2022-11-20 01:23:22 +0600

    add data edit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require( 'express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { response } = require('express');
 require('dotenv').config()
 
 const app = express()
@@ -10,13 +11,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
 
-const uri = `mongodb+srv://${process.env.DB_USER}${process.env.DB_PASSWORD}@cluster0.y5mfc43.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}
+@cluster0.y5mfc43.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
     try{
         const serviceCollection = client.db('therapyCare').collection('services');
+        const orderCollection = client.db('therapyCare').collection('orders');
+       
 
         app.get('/services', async(req, res) =>{
             const query = {}
@@ -30,6 +39,22 @@ async function run(){
             const service = await serviceCollection.findOne(query);
             res.send(service)
 
+        });
+        // orders api //
+        app.get('/orders', async(req, res) =>{
+           let  query = {};
+           if(req.query.email){
+            query = {
+                email:req.query.email
+            }
+           }
+            const cursor = await cursor.toArray();
+            res.send(orders);
+        })
+        app.post('/orders', async(req, res)=>{
+            const orders = req.body;
+            const result = await orderCollection.insertOne(orders)
+            res.send(result);
         })
 
     }
