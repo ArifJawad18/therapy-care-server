59a4a89cad5ae82488f0e74ca0490a9d11a0901a
Arifjawad18 <arif.jawad18@gmail.com>
2023-01-23 02:42:16 +0600

    deleted product

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
   try{
       const serviceCollection = client.db('therapy-care').collection('services');
+      const orderCollection = client.db('therapy-care').collection('orders');
 
       app.get('/services',async(req, res) =>{
         const query = {}
@@ -35,7 +36,46 @@ async function run(){
         const query = {_id: ObjectId(id) };
         const service = await serviceCollection.findOne(query);
         res.send(service);
+      });
+      //orders api
+      app.get('/orders', async(req,res) =>{
+       let query = {}
+        if(req.query.email){
+          query ={
+            email: req.query.email
+          }
+        };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders)
+      });
+      app.post('/orders', async(req, res)=>{
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.send(result);
+
+      });
+
+      app.patch('/orders/:id', async(req, res)=>{
+        const id = req.params.id;
+        const status = req.body.status
+        const query = {_id: ObjectId(id)}
+        const updatedDoc ={
+          $set:{
+            status: status
+          }
+        }
+        const result = await orderCollection.updateOne(query, updatedDoc);
+        res.send(result);
       })
+
+    app.delete('/orders/:id', async(req,res) =>{
+      const id = req.params.id;
+      const filter ={_id: ObjectId(id)};
+      const result = await orderCollection.deleteOne(filter);
+      res.send(result);
+
+    })
   }
   finally{
 
