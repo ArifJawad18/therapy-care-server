3c5559b9ff26be4615255eaa7c1d7ab7de74b211
Arifjawad18 <arif.jawad18@gmail.com>
2023-02-01 03:16:55 +0600

    jwt

--- index.js
+++ index.js
@@ -1,106 +1,128 @@
-const express = require( 'express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const e = require('express');
-
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const e = require("express");
 
 const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
 //middle wares
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bckes0p.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+   return res.status(401).send({message:'unauthorize access'})
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+    if(err){
+      res.status(401).send({message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
 
 
-async function run(){
-  try{
-      const serviceCollection = client.db('therapy-care').collection('services');
-      const orderCollection = client.db('therapy-care').collection('orders');
+}
 
-      app.get('/jwt', (req, res)=>{
+async function run() {
+  try {
+    const serviceCollection = client.db("therapy-care").collection("services");
+    const orderCollection = client.db("therapy-care").collection("orders");
+
+    // get jwt token
+    app.get("/jwt", (req, res) => {
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
-      res.send({token}) 
-    })
-
-
-      app.get('/services',async(req, res) =>{
-        const query = {}
-        const cursor = serviceCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5",
       });
-      app.get('/services/:id', async (req,res)=> {
-        const id = req.params.id;
-        const query = {_id: ObjectId(id) };
-        const service = await serviceCollection.findOne(query);
-        res.send(service);
-      });
-      //orders api
-      app.get('/orders', async(req,res) =>{
-       let query = {}
-        if(req.query.email){
-          query ={
-            email: req.query.email
-          }
-        };
-        const cursor = orderCollection.find(query);
-        const orders = await cursor.toArray();
-        res.send(orders)
-      });
-      app.post('/orders', async(req, res)=>{
-        const order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.send(result);
+      res.send({ token });
+    });
 
-      });
-
-      app.patch('/orders/:id', async(req, res)=>{
-        const id = req.params.id;
-        const status = req.body.status
-        const query = {_id: ObjectId(id)}
-        const updatedDoc ={
-          $set:{
-            status: status
-          }
-        }
-        const result = await orderCollection.updateOne(query, updatedDoc);
-        res.send(result);
-      });
-
-    
-
-    app.delete('/orders/:id', async(req,res) =>{
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const filter ={_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //orders api
+    app.get("/orders", verifyJWT , async (req, res) => {
+      const decoded = req.decoded;
+      console.log('inside orders api', decoded);
+      if(decoded.email !== req.query.email){
+        res.status(403).send({message: 'unauthorized access'})
+      }
+
+      
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await orderCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
       const result = await orderCollection.deleteOne(filter);
       res.send(result);
-
-    })
-  }
-  finally{
-
+    });
+  } finally {
   }
 }
 
-run().catch(err => console.log(err));
+run().catch((err) => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("therapy care server is running");
+});
 
-app.get ('/', (req, res) =>{
-    res.send('therapy care server is running')
-})
-
-app.listen(port, () =>{
-    console.log(`Therapy Care server running on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Therapy Care server running on ${port}`);
+});
